51bf12fbafa62790fc5a2567b5ab1bf0890ab736
hodard <hodardhazwinayo@gmail.com>
2018-03-13 21:09:53 +0200

    database connection updates

--- application/config/database.php
+++ application/config/database.php
@@ -53,7 +53,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'lecteur1994ange';
 $db['default']['database'] = 'smsDB';
 $db['default']['dbdriver'] = 'mysql';
 
